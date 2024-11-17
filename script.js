721f46ba9e7cd9ba5ea49813ea3eafe3c1717efd
lgnq <dzzxzz@gmail.com>
2024-11-17 13:37:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,33 @@ function toggleMenu() {
     const menuIcon = document.querySelector('.menu-icon');
     menuIcon.classList.toggle('open');
 }
+
+const baudRates     = [300, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 74880, 115200, 230400, 250000, 500000, 1000000, 2000000];
+
+const log           = document.getElementById('log');
+const butConnect    = document.getElementById('butConnect');
+
+document.addEventListener('DOMContentLoaded', async () => {
+    butConnect.addEventListener('click', clickConnect);
+  
+    if ('serial' in navigator) {
+      console.log("webserial is supported!")
+    }
+    else
+      console.log("webserial is not supported!")
+  
+    initBaudRate();
+  });
+  
+
+async function clickConnect() {
+}
+
+function initBaudRate() {
+  for (let rate of baudRates) {
+    var option = document.createElement("option");
+    option.text = rate + " Baud";
+    option.value = rate;
+    baudRate.add(option);
+  }
+}
