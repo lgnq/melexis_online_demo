1215784ee68a122191f4a52ab6a36a7db367b5ec
lgnq <dzzxzz@gmail.com>
2024-11-17 13:42:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function toggleMenu() {
     menuIcon.classList.toggle('open');
 }
 
-const baudRates     = [300, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 74880, 115200, 230400, 250000, 500000, 1000000, 2000000];
+const baudRates     = [1200, 2400, 4800, 9600, 19200, 38400, 57600, 74880, 115200, 230400, 250000, 500000, 1000000, 2000000];
 
 const log           = document.getElementById('log');
 const butConnect    = document.getElementById('butConnect');
@@ -21,10 +21,10 @@ document.addEventListener('DOMContentLoaded', async () => {
       console.log("webserial is not supported!")
   
     initBaudRate();
-  });
-  
+});  
 
 async function clickConnect() {
+    console.log("connect...");
 }
 
 function initBaudRate() {
