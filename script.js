f51735b2287e69af64d42f064627410eddddc56a
lgnq <dzzxzz@gmail.com>
2024-11-17 22:08:32 +0800

    Update script.js
    implement all the click functions

--- script.js
+++ script.js
@@ -8,12 +8,20 @@ let outputStream;
 let prefix;
 let separator;
 
+let data = [0, 0, 0];
+
 const maxLogLength  = 50;
 const baudRates     = [1200, 2400, 4800, 9600, 19200, 38400, 57600, 74880, 115200, 230400, 250000, 500000, 1000000, 2000000];
 
 const log           = document.getElementById('log');
 const butConnect    = document.getElementById('butConnect');
+const butClear      = document.getElementById('butClear');
 const baudRate      = document.getElementById('baudRate');
+const autoscroll    = document.getElementById('autoscroll');
+const showTimestamp = document.getElementById('showTimestamp');
+const myInput       = document.getElementById('myInput');
+const sampleSize    = document.getElementById('sampleSize');
+const sampleFreq    = document.getElementById('sampleFreq');
 
 function toggleMenu() {
     const navLinks = document.getElementById('nav-links');
@@ -23,88 +31,48 @@ function toggleMenu() {
     menuIcon.classList.toggle('open');
 }
 
-function saveSetting(setting, value) {
-    window.localStorage.setItem(setting, JSON.stringify(value));
-}
-
-async function changeBaudRate() {
-    saveSetting('baudrate', baudRate.value);
-}
-
-document.addEventListener('DOMContentLoaded', async () => {
-    butConnect.addEventListener('click', clickConnect);
-    baudRate.addEventListener('change', changeBaudRate);
-  
-    if ('serial' in navigator) {
-      console.log("webserial is supported!")
-    }
-    else
-      console.log("webserial is not supported!")
-  
-    initBaudRate();
-});  
-
-async function clickConnect() {
-    if (port) {
-        await disconnect();
-        toggleUIConnected(false);
-        return;
-      }
-    
-      await connect();
-    
-      reset();
-    
-      toggleUIConnected(true);    
-}
-
 async function disconnect() {
-    if (reader) {
-      await reader.cancel();
-      await inputDone.catch(() => {});
-      reader = null;
-      inputDone = null;
-    }
-  
-    if (outputStream) {
-      await outputStream.getWriter().close();
-      await outputDone;
-      outputStream = null;
-      outputDone = null;
-    }
-  
-    await port.close();
-    port = null;
+  if (reader) {
+    await reader.cancel();
+    await inputDone.catch(() => {});
+    reader = null;
+    inputDone = null;
+  }
+
+  if (outputStream) {
+    await outputStream.getWriter().close();
+    await outputDone;
+    outputStream = null;
+    outputDone = null;
+  }
+
+  await port.close();
+  port = null;
 }
 
 function toggleUIConnected(connected) {
-    let lbl = 'Connect';
-  
-    if (connected) {
-      lbl = 'Disconnect';
-    }
-  
-    butConnect.textContent = lbl;
-    
-    // updateTheme()
-}
+  let lbl = 'Connect';
 
-async function reset() {
-    // Clear the data
-    log.innerHTML = "";
+  if (connected) {
+    lbl = 'Disconnect';
+  }
+
+  butConnect.textContent = lbl;
+  
+  // updateTheme()
 }
 
 function logData(line) {
   // Update the Log
-  // if (showTimestamp.checked) {
-  //   let d = new Date();
-  //   let timestamp = d.getHours() + ":" + `${d.getMinutes()}`.padStart(2, 0) + ":" +
-  //       `${d.getSeconds()}`.padStart(2, 0) + "." + `${d.getMilliseconds()}`.padStart(3, 0);
+  if (showTimestamp.checked) {
+    let d = new Date();
+    let timestamp = d.getHours() + ":" + `${d.getMinutes()}`.padStart(2, 0) + ":" +
+        `${d.getSeconds()}`.padStart(2, 0) + "." + `${d.getMilliseconds()}`.padStart(3, 0);
 
-  //   log.innerHTML += '<span class="timestamp">' + timestamp + ' -> </span>';
+    log.innerHTML += '<span class="timestamp">' + timestamp + ' -> </span>';
     
-  //   d = null;
-  // }
+    d = null;
+  }
 
   log.innerHTML += line+ "<br>";
 
@@ -115,74 +83,113 @@ function logData(line) {
     log.innerHTML = logLines.splice(-maxLogLength).join("<br>\n");
   }
 
-  // if (autoscroll.checked) {
+  if (autoscroll.checked) {
     log.scrollTop = log.scrollHeight;
-  // }
+  }
 }
 
 class LineBreakTransformer {
-    constructor() {
-      // A container for holding stream data until a new line.
-      this.container = '';
-    }
-  
-    transform(chunk, controller) {
-      this.container += chunk;
-      const lines = this.container.split('\n');
-      this.container = lines.pop();
-      lines.forEach(line => {
-        controller.enqueue(line)
-        logData(line);
-      });
-    }
-  
-    flush(controller) {
-      controller.enqueue(this.container);
-    }
-}
+  constructor() {
+    // A container for holding stream data until a new line.
+    this.container = '';
+  }
 
-async function connect() {
-    // - Request a port and open a connection.
-    port = await navigator.serial.requestPort();
-  
-    // - Wait for the port to open.toggleUIConnected
-    await port.open({ baudRate: baudRate.value });
-  
-    let decoder = new TextDecoderStream();
-    inputDone   = port.readable.pipeTo(decoder.writable);
-    inputStream = decoder.readable.pipeThrough(new TransformStream(new LineBreakTransformer()));
-  
-    const encoder = new TextEncoderStream();
-    outputDone    = encoder.readable.pipeTo(port.writable);
-    outputStream  = encoder.writable;
-  
-    reader = inputStream.getReader();
-  
-    // prefix    = document.getElementById('messageprefixid').value
-    // separator = document.getElementById('messageseparatorid').value
-  
-    readLoop().catch(async function(error) {
-      toggleUIConnected(false);
-      await disconnect();
+  transform(chunk, controller) {
+    this.container += chunk;
+    const lines = this.container.split('\n');
+    this.container = lines.pop();
+    lines.forEach(line => {
+      controller.enqueue(line)
+      logData(line);
     });
+  }
+
+  flush(controller) {
+    controller.enqueue(this.container);
+  }
 }
 
 async function readLoop() {
-    while (true) {
-      const {value, done} = await reader.read();
-        
-      if (value) {
-    //     if (value.substr(0, prefix.length) == prefix) {
-    //       orientations = value.substr(prefix.length).trim().split(separator).map(x=>+x);
-    //     }
-      }
-  
-      if (done) {
-        console.log('[readLoop] DONE', done);
-        reader.releaseLock();
-        break;
+  while (true) {
+    const {value, done} = await reader.read();
+      
+    if (value) {
+      if (value.substr(0, prefix.length) == prefix) {
+        data = value.substr(prefix.length).trim().split(separator).map(x=>+x);
       }
     }
+
+    if (done) {
+      console.log('[readLoop] DONE', done);
+      reader.releaseLock();
+      break;
+    }
+  }
+}
+
+async function connect() {
+  // - Request a port and open a connection.
+  port = await navigator.serial.requestPort();
+
+  // - Wait for the port to open.toggleUIConnected
+  await port.open({ baudRate: baudRate.value });
+
+  let decoder = new TextDecoderStream();
+  inputDone   = port.readable.pipeTo(decoder.writable);
+  inputStream = decoder.readable.pipeThrough(new TransformStream(new LineBreakTransformer()));
+
+  const encoder = new TextEncoderStream();
+  outputDone    = encoder.readable.pipeTo(port.writable);
+  outputStream  = encoder.writable;
+
+  reader = inputStream.getReader();
+
+  prefix    = document.getElementById('messageprefixid').value
+  separator = document.getElementById('messageseparatorid').value
+
+  readLoop().catch(async function(error) {
+    toggleUIConnected(false);
+    await disconnect();
+  });
+}
+
+async function reset() {
+  // Clear the data
+  log.innerHTML = "";
+}
+
+async function clickConnect() {
+  if (port) {
+      await disconnect();
+      toggleUIConnected(false);
+      return;
+    }
+  
+    await connect();
+  
+    reset();
+  
+    toggleUIConnected(true);    
+}
+
+function saveSetting(setting, value) {
+    window.localStorage.setItem(setting, JSON.stringify(value));
+}
+
+async function changeBaudRate() {
+    saveSetting('baudrate', baudRate.value);
+}
+
+async function clickClear() {
+  reset();
+}
+
+async function clickAutoscroll() {
+  saveSetting('autoscroll', autoscroll.checked);
+}
+
+async function clickTimestamp() {
+  saveSetting('timestamp', showTimestamp.checked);
 }
 
 function initBaudRate() {
@@ -193,3 +200,40 @@ function initBaudRate() {
     baudRate.add(option);
   }
 }
+
+function loadSetting(setting, defaultValue) {
+  let value = JSON.parse(window.localStorage.getItem(setting));
+
+  if (value == null) {
+    return defaultValue;
+  }
+
+  return value;
+}
+
+function loadAllSettings() {
+  // Load all saved settings or defaults
+  autoscroll.checked    = loadSetting('autoscroll', true);
+  showTimestamp.checked = loadSetting('timestamp', false);
+  // kalmanFilter.checked  = loadSetting('kalmanfilter', false);
+  baudRate.value        = loadSetting('baudrate', 9600);
+  // darkMode.checked      = loadSetting('darkmode', false);
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+  butConnect.addEventListener('click', clickConnect);
+  baudRate.addEventListener('change', changeBaudRate);
+  butClear.addEventListener('click', clickClear);
+  autoscroll.addEventListener('click', clickAutoscroll);
+  showTimestamp.addEventListener('click', clickTimestamp);
+  baudRate.addEventListener('change', changeBaudRate);
+
+  if ('serial' in navigator) {
+    console.log("webserial is supported!")
+  }
+  else
+    console.log("webserial is not supported!")
+
+  initBaudRate();
+  loadAllSettings();
+});  
